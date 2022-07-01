32e1e4dba1664ebc395dd42805e742dc73d6b4ec
wrynnsun <wrynnsun@tencent.com>
2022-07-01 15:45:32 +0800

    path 的配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
   extends: ['./eslint/vue3-ts-typecheck.js', 'prettier'],
+  overrides: [
+    {
+      files: ['vite.config.ts'],
+      env: {
+        node: true,
+      },
+      parserOptions: {
+        project: null,
+      },
+    },
+  ],
 };

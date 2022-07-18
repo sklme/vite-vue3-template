66652ac7ff02b2acadeacdc822a72ccdb116d985
wrynnsun <wrynnsun@tencent.com>
2022-07-18 20:55:48 +0800

    feat: router

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,22 +1,53 @@
 import { RouteRecordRaw } from 'vue-router';
-import WindiCSSVue from '~/components/preset/WindiCSS/WindiCSS.vue';
-import Readme from '../readme.md';
+
+const feature: RouteRecordRaw = {
+  path: '/feature',
+  name: 'Feature',
+  component: {},
+  children: [
+    {
+      path: '/windicss',
+      name: 'Windicss',
+      component: {},
+    },
+    {
+      path: '/md',
+      name: 'Markdown',
+      component: {},
+    },
+    {
+      path: '/pinia',
+      name: 'Pinia',
+      component: {},
+    },
+    {
+      path: '/vitest',
+      name: 'Vitest',
+      component: {},
+    },
+    {
+      path: '/vueuse',
+      name: 'Vueuse',
+      component: {},
+    },
+    {
+      /*       
+      "unplugin-auto-import": "^0.9.2",
+      "unplugin-vue-components": "^0.21.0", 
+      */
+      path: '/unplugin',
+      name: '',
+      component: {},
+    },
+  ],
+};
 
 export const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'Index',
-    component: Readme,
+    component: {},
   },
-  {
-    path: 'test',
-    name: 'Test',
-    children: [
-      {
-        path: '/windicss',
-        name: 'Windicss',
-        component: WindiCSSVue,
-      },
-    ],
-  },
+  // 功能
+  feature,
 ];
